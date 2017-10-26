e793833aef989341e00d01abaae8f05351b6cb7b
Zaur Giyasov <zagiyas@mera.ru>
2017-10-26 11:00:53 +0300

    Create project

---
+++ Notes/Notes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Zaur Giyasov on 26/10/2017.
+//  Copyright © 2017 ZaurGiyasov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
