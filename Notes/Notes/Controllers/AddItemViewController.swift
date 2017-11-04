a51837c431e2ee5d97470ea877d413cd6684072a
Zaur Giyasov <zaurusnnov@icloud.com>
2017-11-04 11:04:38 +0300

    Create Add Item scene Embedet it to NaviController Create new controller file and associate to new scene (Add Item)

---
+++ Notes/Notes/Controllers/AddItemViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddItemViewController.swift
+//  Notes
+//
+//  Created by ZaurNNov on 04.11.2017.
+//  Copyright © 2017 ZaurGiyasov. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    //Controller for Add Item scene
+    //(This View create new Item(note)
+
+    //MARK: Action and IB elements
+    @IBAction func cancel()
+    {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func done()
+    {
+        dismiss(animated: true, completion: nil)
+    }
+
+    
+
+}
