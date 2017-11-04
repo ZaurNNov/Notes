85e9b647edc754781d02fff54e0e5e20cee76d59
Zaur Giyasov <zaurusnnov@icloud.com>
2017-11-04 12:05:07 +0300

    add textField in row delegate it to controller add func hide/unhide done button if textfield text empty

--- Notes/Notes/Controllers/AddItemViewController.swift
+++ Notes/Notes/Controllers/AddItemViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
 
     //Controller for Add Item scene
     //(This View create new Item(note)
@@ -24,6 +24,33 @@ class AddItemViewController: UITableViewController {
         dismiss(animated: true, completion: nil)
     }
 
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    @IBOutlet weak var textField: UITextField!
+
     
 
+    //MARK: TableView Delegate
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+
+    //MARK: TableView Life cicle methods
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+
+    //MARK: UITextFieldDelegate funcs
+    //if text field empty - hide Done buttom
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+
+        let oldText = textField.text! as NSString
+        let newText = oldText.replacingCharacters(in: range, with: string) as NSString
+        doneBarButton.isEnabled = (newText.length > 0)
+
+        return true
+    }
+
+    
+    
 }
