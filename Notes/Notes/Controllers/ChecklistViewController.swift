20bc58a6d7e9fc6ae4ab2697e570c91bf5a3d64d
Zaur Giyasov <zaurusnnov@icloud.com>
2017-11-09 07:36:56 +0300

    Add path to App-HomeDirectory folder Save data in the .plist file

--- Notes/Notes/Controllers/ChecklistViewController.swift
+++ Notes/Notes/Controllers/ChecklistViewController.swift
@@ -41,6 +41,7 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
         items.append(row4text)
 
         super.init(coder: aDecoder)
+        print("Documents folder is \(documentsDirectory())")
     }
     
     //my id for cell and segue
@@ -89,6 +90,8 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
             configureCheckmark(for: cell, with: item)
         }
         tableView.deselectRow(at: indexPath, animated: true)
+        //save in folder
+        saveChecklistItems()
     }
 
     func configureCheckmark(for cell: UITableViewCell,
@@ -117,6 +120,8 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
         items.remove(at: indexPath.row)
         let indexPaths = [indexPath]
         tableView.deleteRows(at: indexPaths, with: .automatic)
+        //save in folder
+        saveChecklistItems()
     }
 
     //MARK: Delegates: hand-in-hund with protocols
@@ -136,9 +141,12 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
         tableView.insertRows(at: indexPaths, with: .automatic)
 
         dismiss(animated: true, completion: nil)
+        //save in folder
+        saveChecklistItems()
     }
 
-    func itemDetailViewController(_ controller: ItemDetailViewController, didFinishEditing item: ChecklistItem) {
+    func itemDetailViewController(_ controller: ItemDetailViewController, didFinishEditing item: ChecklistItem)
+    {
         if let index = items.index(of: item) {
             let indexPath = IndexPath(row: index, section: 0)
             if let cell = tableView.cellForRow(at: indexPath) {
@@ -146,10 +154,13 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
             }
         }
         dismiss(animated: true, completion: nil)
+        //save in folder
+        saveChecklistItems()
     }
 
     //MARK: Segues
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
         //Delegate: ItemDetailViewController
         if segue.identifier == variableIdentifiers.addItem {
             //Add new Item
@@ -166,6 +177,29 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
                 targetController.itemToEdit = items[indexPath.row]
             }
         }
+    }
+
+    //MARK: DocumentDirectory save my files
+    func documentsDirectory() -> URL
+    {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        //link in this app-home folder
+        return paths[0]
+    }
+
+    func dataFilePath() -> URL
+    {
+        return documentsDirectory().appendingPathComponent("Checklists.plist")
+    }
+
+    //serialization
+    func saveChecklistItems()
+    {
+        let data = NSMutableData()
+        let arhiver = NSKeyedArchiver(forWritingWith: data)
+        arhiver.encode(items, forKey: "ChecklistItems")
+        arhiver.finishEncoding()
+        data.write(to: dataFilePath(), atomically: true)
     }
 
 
--- Notes/Notes/Model/ChecklistItem.swift
+++ Notes/Notes/Model/ChecklistItem.swift
@@ -9,12 +9,26 @@
 import Foundation
 
 //Model
-class ChecklistItem: NSObject {
+class ChecklistItem: NSObject, NSCoding {
     var text = ""
     var checked = false
 
     func toggleChecked() {
         checked = !checked
+    }
+
+    //Coding - Decoding
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(text, forKey: "Text")
+        aCoder.encode(checked, forKey: "Checked")
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init()
+    }
+
+    override init() {
+        super.init()
     }
 }
 
